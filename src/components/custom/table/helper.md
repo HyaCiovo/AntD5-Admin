0a48f86a54f090e1c6be01d358d9892018276c88
zhujiruo <zhujiruo@foxmail.com>
2024-07-10 23:25:01 +0800

    docs: update author info and version in package.json
    - Add author information including name, email, and GitHub link.
    - Bump version number from 0.0.0 to 0.0.1 to reflect minor changes.

--- src/components/custom/table/helper.md
+++ src/components/custom/table/helper.md
@@ -1,8 +1,8 @@
 # TableWithFilters
 
->  基于 [Ant Design 5.0](https://ant.design/index-cn) 组件库，使用 json 数组直接生成带有过滤条件的 Table 组件。
+> 基于 [Ant Design 5.0](https://ant.design/index-cn) 组件库，使用 json 数组直接生成带有过滤条件的 Table 组件。
 >
->  过滤条件、数据请求和数据展示通过 [useAntdTable - ahooks 3.0](https://ahooks.js.org/zh-CN/hooks/use-antd-table) 实现。
+> 过滤条件、数据请求和数据展示通过 [useAntdTable - ahooks 3.0](https://ahooks.js.org/zh-CN/hooks/use-antd-table) 实现。
 
 ## 代码示例
 
@@ -35,15 +35,13 @@ const TablePage = () => {
 
 <img src="../../../assets/images/demo/image-20240710163624526.png" alt="image" style="zoom: 50%;" />
 
-## 当前支持的 FilterItem 
+## 当前支持的 FilterItem
 
 1. InputFilter
 2. SelectFilter
 3. DateFilter
 4. RangeFilter
 5. TreeFilter
-
-
 
 ## 待扩展
 
--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,20 +1,19 @@
-import { useUserStore } from '@/stores/user';
-import { Avatar, Dropdown, MenuProps } from 'antd';
-import { VITE_APP_LOGO, VITE_APP_TITLE } from 'config/constant';
-import { Link } from 'react-router-dom';
+import { useUserStore } from "@/stores/user";
+import { Avatar, Dropdown, MenuProps } from "antd";
+import { VITE_APP_LOGO, VITE_APP_TITLE } from "config/constant";
+import { Link } from "react-router-dom";
 const Header = () => {
-  const user = useUserStore()
-  const url = 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg';
-  const handleLogout = () => { }
-  const items: MenuProps['items'] = [
+  const user = useUserStore();
+  const handleLogout = () => {};
+  const items: MenuProps["items"] = [
     {
-      key: '0',
-      label: '用户名',
-      type: 'group'
+      key: "0",
+      label: "用户名",
+      type: "group",
     },
     {
-      key: '1',
-      label: '退出登录',
+      key: "1",
+      label: "退出登录",
       onClick: handleLogout,
     },
   ];
@@ -23,11 +22,7 @@ const Header = () => {
     <div className="bg-primary w-screen flex h-16 px-6 text-white justify-between select-none">
       <div className="flex items-center font-comic text-xl">
         <Link to="/" className="flex items-center">
-          <img
-            src={VITE_APP_LOGO}
-            alt="logo"
-            className="h-8"
-          />
+          <img src={VITE_APP_LOGO} alt="logo" className="h-8" />
           <h1 className="font-semibold ml-2">Ant Design 5</h1>
         </Link>
         <span className="opacity-50 text-base mx-6">|</span>
@@ -39,11 +34,15 @@ const Header = () => {
           placement="bottomRight"
           getPopupContainer={() => document.body}
         >
-          <Avatar size={32} src={url} className="cursor-pointer" />
+          <Avatar
+            size={32}
+            src={"/avatar/hyacinth_avatar.jpg"}
+            className="cursor-pointer"
+          />
         </Dropdown>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
